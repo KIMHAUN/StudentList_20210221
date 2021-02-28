bad9eb000c2edc8c275660c8cdf8413b257ee07d
kimhaun <wha02068@gmail.com>
2021-02-28 11:25:38 +0900

    어댑터 xml Arraylist 조합 리스트뷰에 부착
    어댑터 - getView 함수 수정 (실 데이터 반영)
    but 나이는 아직 제대로 계산되지 않음
    
    실제 나이 계산 로직 추가
    
    리스트뷰 클릭 이벤트 추가
    
    롱클릭 추가 이벤트
    
    롱클릭 이벤트 변경 - 목록 제거 / 리스트뷰 새로 고침
    
    롱클릭 이벤트 변경 - 경고창 확인시에만 삭제 진행

--- app/src/main/java/kr/co/neoplus/studentlist_20210221/MainActivity.kt
+++ app/src/main/java/kr/co/neoplus/studentlist_20210221/MainActivity.kt
@@ -1,17 +1,23 @@
 package kr.co.neoplus.studentlist_20210221
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.neoplus.studentlist_20210221.adapters.StudentAdapter
 import kr.co.neoplus.studentlist_20210221.datas.Student
 
 class MainActivity : AppCompatActivity() {
 
     val mStudentList = ArrayList<Student>()
 
+    lateinit var mAdapter : StudentAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         mStudentList.add(Student("조경진", 1988))
         mStudentList.add(Student("강서영", 1990))
@@ -24,6 +30,45 @@ class MainActivity : AppCompatActivity() {
         mStudentList.add(Student("채명호", 1973))
         mStudentList.add(Student("최원종", 1967))
         mStudentList.add(Student("최윤정", 1987))
+
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+
+        studentListView.adapter = mAdapter
+
+        studentListView.setOnItemClickListener { parent, view, position, id ->
+
+            val clickedStudent = mStudentList[position]
+            Toast.makeText(this, clickedStudent.name, Toast.LENGTH_SHORT).show()
+
+        }
+
+        studentListView.setOnItemLongClickListener { parent, view, position, id ->
+
+//            val std = mStudentList[position]
+//            Toast.makeText(this, "${std.name} 길게 눌림", Toast.LENGTH_SHORT).show()
+
+            val alert = AlertDialog.Builder(this)
+            alert.setTitle("삭제 확인")
+            alert.setMessage("정말 삭제하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+                mStudentList.removeAt(position)
+                mAdapter.notifyDataSetChanged()
+            })
+            alert.setNegativeButton("아니오", null)
+            alert.show()
+
+            return@setOnItemLongClickListener true
+
+        }
+
+
+
+
+
+
+
+
+
 
     }
 }
--- app/src/main/java/kr/co/neoplus/studentlist_20210221/adapters/StudentAdapter.kt
+++ app/src/main/java/kr/co/neoplus/studentlist_20210221/adapters/StudentAdapter.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.CalendarView
+import android.widget.TextView
 import kr.co.neoplus.studentlist_20210221.R
 import kr.co.neoplus.studentlist_20210221.datas.Student
 import java.sql.RowId
+import java.util.*
+import kotlin.collections.ArrayList
 
 class StudentAdapter(
     val mContext : Context,
@@ -24,6 +28,21 @@ class StudentAdapter(
         }
 
         val row = tempRow!!
+
+        val student = mList[position]
+
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
+
+        nameTxt.text = student.name
+        ageTxt.text = "(${student.birthYear}세)"
+
+        val cal = Calendar.getInstance()
+        val age = cal.get(Calendar.YEAR) - student.birthYear + 1
+
+        ageTxt.text = "(${age}세)"
+
+
 
         return row
 
